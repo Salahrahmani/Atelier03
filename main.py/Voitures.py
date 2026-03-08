49b79cfa2b397326ff27c4b6b4465edd377fc29e
SALAH <salaheddine.rahmani@monborial.ca>
2026-03-08 19:15:48 -0400

    Ajouter La methode sortir_Voiture

--- main.py/Voitures.py
+++ main.py/Voitures.py
@@ -16,10 +16,19 @@ class Parking:
         if voiture in self.voitures:
             print("La Voiture Deja existe Dans le Parking")
             return
-
         if len(self.voitures) >= self.capacite:
             print(" PARDON *Le Parking Plein* ")
             return
 
         self.voitures.append(voiture)
         print("Le Voiture a été ajouté ")
+
+    def sortirVoiture(self, voiture):
+
+        if voiture not in self.voitures:
+            print("***La Voiture n'est pas dans Parking***")
+            return
+
+        self.voitures.remove(voiture)
+        print("Voiture retiree")
+        print("Places libres:", self.calculerNbrPlacesLibres())
